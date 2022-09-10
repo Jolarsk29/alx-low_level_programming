46b583b933288d97b71ab236f000941dbdf84598
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-09-09 22:13:30 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,39 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- *
- *
- * Description: print
- *
- *
- *
+ * main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
+ * Return: Always 0 (Success)
  */
-int main ()
+ 
+int main(void)
 {
-	int n1 = 0, n2;
-	while(n1 <= 99)
+	int tens;
+	int ones;
+	int t;
+	int o;
+
+	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
 	{
-		n2 = n1
-			while (n2 <= 99)
+		for (ones = '0'; ones <= '9'; 0nes++)
+		{
+			for (t = tens; t <= '9'; t++) /*print second of pair*/
 			{
-				if (n2 != n1)
+				for (o = ones + 1; o <= '9'; o++)
 				{
-					putchar((n1 / 10) + 48);
-					putchar((n1 % 10) + 48);
+					putchar(tens);
+					putchar(ones);
 					putchar(' ');
-					putchar((n2 / 10) + 48);
-					putchar((n2 % 10) + 48);
-					
-					if (n1 !=98 || n2 !=98)
+					putchar(t);
+					putchar(o);
+
+					if (!((tens == '9' && ones == '8') &&
+						(t == '9' && o == '9')))
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
-				++n2;
+				o = '0';
 			}
-			++n1;
 		}
-		putchar('\n');
-		return (0);
+	}
+	putchar('\n');
+
+	return (0);
 }
