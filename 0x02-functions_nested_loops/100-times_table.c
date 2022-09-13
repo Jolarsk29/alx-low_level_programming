73284d7be96c6128d724ac41b89cb3a41a09142f
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-09-13 13:25:24 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,6 @@
 void print_times_table(int n)
 
 {
-
 	int num, mult, prod;
 
 	if (n >= 0 && n <= 15)
@@ -40,24 +39,16 @@ void print_times_table(int n)
 				if (prod >= 100)
 
 				{
-
 					_putchar((prod / 100) + '0');
-
 					_putchar(((prod / 10)) % 10 + '0');
-
 				}
-
 				else if (prod <= 99 && prod >= 10)
-
 				{
-
 					_putchar((prod / 10) + '0');
 
 				}
-
 				_putchar((prod % 10) + '0');
 			}
-
 			_putchar('\n');
 		}
 	}
