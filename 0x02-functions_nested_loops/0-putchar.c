fa798e6fa281099216048ed9681ec16758f5a1d4
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-09-13 09:44:55 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,3 @@
-#include <main.h>
-
 #include <stdio.h>
 
 /**
@@ -9,14 +7,15 @@
 
 int main(void)
 {
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	-putchar('\n');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
 	return (0);
 }
