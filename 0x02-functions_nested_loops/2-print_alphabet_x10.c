aede564023744174d99f40d695d58dedeabf3983
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-09-13 12:05:00 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,7 @@
 #include "main.h"
 
-
-
 /**
-
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
-
  */
 
 void print_alphabet_x10(void)
@@ -21,12 +17,11 @@ void print_alphabet_x10(void)
 	while (count++ <= 9)
 
 	{
-
 		for (letter = 'a'; letter <= 'z'; letter++)
-
+		
 			_putchar(letter);
-
+		
 		_putchar('\n');
 
 	}
-
+}
