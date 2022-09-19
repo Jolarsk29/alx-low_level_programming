b404a50ed3bb078da022e6e7dda530eec34b4812
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-09-19 14:08:52 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,14 @@
 #include "main.h"
 
 /**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: string to print
+ * _puts - prints a string to stdout.
+ * @str: The string to be printed.
  */
 
 void _puts(char *str)
-
 {
-
 	while (*str)
-
 		_putchar(*str++);
 
 	_putchar('\n');
-
 }
