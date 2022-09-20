287b3b4a25c8504a19752da7ee94dd3105f4384a
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-09-19 21:30:21 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,35 +1,27 @@
 #include "main.h"
 
 /**
- * puts_half - function prints half of a string
- *
- *@str: char pointer to string
- *
- * Return: 0
- */
+*puts_half - puts half of a string
+*@str: string
+*
+*Return:void
+*/
 
 void puts_half(char *str)
 {
+
 	int len = _strlen(str);
-
-	int y;
-
+	
 	if (len % 2 == 0)
 
+		len = len / 2;
+	else
+		len = (len + 1) / 2;
+	
+	while (str[len] != '\0')
 	{
-		y = len / 2;
-	}
-
-	else if (len % 2 != 0)
-
-	{
-		y = (len + 1) / 2;
-	}
-
-	for (; *(str + y) != '\0'; y++)
-
-	{
-		_putchar(*(str + y));
+		_putchar(str[len]);
+		len++;
 	}
 
 	_putchar('\n');
