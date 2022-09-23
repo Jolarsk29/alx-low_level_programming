fdb700e0295f8fd363b9856091b6143c981ff853
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-09-22 19:05:06 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,40 +7,20 @@
  * Return: address of the encoded string
  */
 
-char *leet(char *str)
+char *leet(char *s)
 {
-	int i = 0;
+	int i, j;
+	
+	char a[] = "aAeEoOtTll";
+	char b[] = "4433007711";
 
-	while (str[i] != '\0')
+	for (i = 0; *(s + i); i++)
 	{
-		str[i] = transform(str[i]);
-		i++;
-	}
-	return (str);
-}
-
-/**
- * transform - helper function to map a letter with it's leet encoding
- * @x: char to be encoded
- *
- * Return: the encoded char
- */
-
-char transform(char x)
-{
-	char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
-	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
-	int i = 0;
-	char replacement = x;
-
-	while (i < 8)
-	{
-		if (x == mapping_low[i] || x == mapping_upper[i])
+		for (j = o; j <= 9; j++)
 		{
-			replacement = i + 'e';
-			break;
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
 		}
-		i++;
 	}
-	return (replacement);
+	return (s);
 }
