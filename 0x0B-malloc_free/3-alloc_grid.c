4c60d0b6d2cad52b927443362b476b97380ee765
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-10-04 10:53:53 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,25 +10,20 @@
 */
 
 int **alloc_grid(int width, int height)
-
 {
 	int **array;
 	int i = 0, j;
 
 	if (width == 0 || height == 0)
-
 		return (NULL);
 
 	array = (int **) malloc(sizeof(int *) * height);
 	if (array != NULL)
-
 	{
 		for (; i < height; i++)
-
 		{
 			array[i] = (int *) malloc(sizeof(int) * width);
 			if (array[i] != NULL)
-
 			{
 				for (j = 0; j < width; j++)
 					array[i][j] = 0;
@@ -36,14 +31,12 @@ int **alloc_grid(int width, int height)
 			else
 			{
 				while (i >= 0)
-
 				{
 					free(array[i]);
 					i--;
 				}
 
 				free(array);
-
 				return (NULL);
 			}
 		}
