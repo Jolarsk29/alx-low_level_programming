44931d5a9d00f299d39aaee6cf197fb95e862d08
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-10-10 13:57:46 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,7 @@
 #include "dog.h"
 
 /**
-* init_dog - inits a variable of
-*type dog
+* init_dog - inits a variable of type dog
 *@d: dog identification
 *@name: name of dog
 *@age: age of dog
@@ -13,8 +12,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 
 {
 
-	if (d != NULL
-			)	
+	if (d != NULL)
+
 	{
 		(*d).name = name;			
 
