845760d62d8e089e824ca1403fe881b1c0540738
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-10-10 21:36:20 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,7 +11,7 @@ int _strlen(char *str)
 
 {
 	int len = 0;
-	
+
 	while (*str++)
 		len++;
 
@@ -30,7 +30,7 @@ char *_strcopy(char *dest, char *src)
 
 {
 	int index = 0;
-	
+
 	for (index = 0; src[index]; index++)
 		dest[index] = src[index];
 	dest[index] = '\0';
@@ -49,16 +49,15 @@ char *_strcopy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 
 {
+
 	dog_t *doggo;
-	
+
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 	doggo = malloc(sizeof(dog_t));
-	
 	if (doggo == NULL)
 		return (NULL);
 	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
-	
 	if (doggo->name == NULL)
 	{
 		free(doggo);
