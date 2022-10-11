a215d71d4701d410ab30b04d54de22ab2ee80b73
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-10-10 21:05:03 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,15 +11,13 @@
 void print_dog(struct dog *d)
 
 {
-	if (d == NULL )
+	if (d == NULL)
 		return;
 
 	if (d->name == NULL)
-		printf("Name: (nil\n");
+		printf("Name: (nil)\n");
 	else
-
 		printf("Name: %s\n", d->name);
-	
 	if (d->age < 0)
 		printf("Age: (nil)\n");
 	else
