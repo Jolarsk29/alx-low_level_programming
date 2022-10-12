b83004231e7f5fb05f1175d48faa2022ceed8dcd
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-10-12 15:18:43 -0700

    3-calc.h

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -25,7 +25,6 @@ int (*get_op_func(char *s))(int, int)
 
 		{NULL, NULL}
 	};
-	
 	int i = 0;
 
 	while (ops[i].op != NULL)
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,7 +12,6 @@
 int main(int argc, char *argv[])
 
 {
-
 	int (*funoperator)(int a, int b);
 
 	/* when the user enter the arguments there´s must be four elements */
@@ -21,13 +20,10 @@ int main(int argc, char *argv[])
 	/* if the number or arguments is wrong, print error */
 
 	if (argc != 4)
-
 	{
 		printf("Error\n");
-
 		exit(98);
 	}
-
 	/* funoperator is gonna be equal to the pointer function */
 	/* in the argument number 2 that is the position of the operator */
 	/*if is not one of the operators mentioned else print Error */
@@ -35,13 +31,10 @@ int main(int argc, char *argv[])
 	funoperator = get_op_func(argv[2]);
 
 	if (!funoperator || strlen(argv[2]) != 1)
-
 	{
 		printf("Error\n");
-
 		exit(99);
 	}
-
 	/* add last condition, if user try to divides using % or / is Error */
 	/* that means, if *argv in position 2 that are operators */
 	/* receives % or / operators program prints Error */
@@ -49,16 +42,12 @@ int main(int argc, char *argv[])
 	/* e.g 2  % 0 , if argv in position 3is equal to cero is Error */
 
 	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
-
 	{
 		printf("Error\n");
-
 		exit(100);
 	}
-
 	/* if any of the conditions theeennn : */
 
 	printf("%d\n", funoperator(atoi(argv[1]), atoi(argv[3])));
-
 	return (0);
 }
