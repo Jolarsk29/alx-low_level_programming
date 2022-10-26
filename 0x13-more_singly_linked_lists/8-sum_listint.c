ccc12b7e188d4bfaaff8422af4fac8c3b709fd02
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-10-25 18:46:14 -0700

    mandatory task

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,53 +1,28 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * sum_listint - sum all the data (n) of listint_t list
- * @head: a pointer to the head of the list
- * Return: 0 if the list is empty or the sum of all the data
+ *sum_listint - gets the sum of the data of a listint list
+ *@head:pointer to list
+ *Return:sum
  */
 
 int sum_listint(listint_t *head)
 
 {
-
 	int sum = 0;
+
+	if (head == NULL)
+
+		return (0);
 
 	while (head)
 
 	{
-		/* find the sum of nodes */
-		sumofnodes(head, &sum);
-		/* required sum */
-		return (sum);
+		sum += head->n;
+		head = head->next;
 	}
-	return (0);
-}
-/**
- * sumofnodes - find the sum of ndoes
- * @head: a pointer to the head of the lintint_t list
- * @sum: integer that sums
- * Description: function to recursively find the sum of nodes
- * of the given linked list
- * Return: the sum
- */
 
-void sumofnodes(listint_t *head, int *sum)
-
-{
-
-	int n;
-
-	/* if head is equal to NULL */
-
-	if (!head)
-
-		return;
-
-	/* de reference to n to the data */
-	/* so I can make the sum */
-	n = head->n;
-	/* recursively traverse the remaining nodes  */
-	sumofnodes(head->next, sum);
-	/* accumulate sum */
-	*sum = *sum + n;
+	return (sum);
 }
