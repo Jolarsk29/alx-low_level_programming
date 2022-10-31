a43033ba81b9b0384604140ef64d5212411f8344
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-10-31 13:15:19 -0700

    manatory tasks

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,12 +8,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-/**
-*void prototypes()
-*int prototypes()
-*char prototypes()
-*/
-
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
