57c934cfac09de6f8ca20f16236d4e39d9928583
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-11-03 01:25:45 -0700

    mandatory tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,59 @@
 #include "main.h"
 
+#include <stddef.h>
+
+/**
+ * _strlen - counts string length
+ * @str: string to be used
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *str)
+
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
 /**
  * create_file - creates a file
- * @filename: the name of the file to create
- * @text_content: a NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure
+ * @filename: name of the file
+ * @text_content: content of the file to be created
+ *
+ * Return: 1 on success, -1 otherwise
  */
 
 int create_file(const char *filename, char *text_content)
 
 {
-    int file, fwrite, i;
+	int file, wrote;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+	if (text_content != NULL)
 
-    file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	{
+		wrote = write(file, text_content, _strlen(text_content));
 
-    if (file == -1)
-        return (-1);
+		if (wrote == -1)
 
-    if (text_content != NULL)
-    {
-        for (i = 0; text_content[i]; i++)
-            ;
-        fwrite = write(file, text_content, i);
-        if (fwrite == -1)
-            return (-1);
-    }
-    close(file);
-
-    return (1);
+		{
+			close(file);
+			return (-1);
+		}
+		close(file);
+		return (1);
+	}
+	else
+	{
+		close(file);
+		return (1);
+	}
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,35 +1,59 @@
 #include "main.h"
 
+#include <stddef.h>
+
+/**
+ * _strlen - counts string length
+ * @str: string to be used
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *str)
+
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
 /**
  * append_text_to_file - appends text at the end of a file
- * @filename: the name of the file
- * @text_content: the NULL terminated string to add at end of file
- * Return: 1 on success, -1 on failure
+ * @filename: name of the file
+ * @text_content: content to be appended
+ *
+ * Return: 1 on success, -1 otherwise
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 
 {
-    int file, fwrite, i;
+	int file, wrote;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+	if (text_content != NULL)
 
-    file = open(filename, O_APPEND | O_RDWR, 0664);
-    if (file == -1)
-        return (-1);
+	{
+		wrote = write(file, text_content, _strlen(text_content));
 
-    if (text_content != NULL)
+		if (wrote == -1)
 
-    {
-        for (i = 0; text_content[i]; i++)
-            ;
-        fwrite = write(file, text_content, i);
-
-        if (fwrite == -1)
-            return (-1);
-    }
-    close(file);
-
-    return (1);
+		{
+			close(file);
+			return (-1);
+		}
+		close(file);
+		return (1);
+	}
+	else
+	{
+		close(file);
+		return (1);
+	}
 }
