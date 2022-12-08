6a4a68cced55c83e2a906e3d0613dbd5a2deb069
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-12-08 05:48:23 -0800

    mandatory task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,23 +11,19 @@
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
-
 {
 	dlistint_t *new;
-
 	dlistint_t *head;
 
 	unsigned int i;
 
 	new = NULL;
-
 	if (idx == 0)
 		new = add_dnodeint(h, n);
 	else
 	{
 		head = *h;
 		i = 1;
-		
 		if (head != NULL)
 			while (head->prev != NULL)
 				head = head->prev;
@@ -35,7 +31,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			if (i == idx)
 			{
-
 				if (head->next == NULL)
 					new = add_dnodeint_end(h, n);
 				else
